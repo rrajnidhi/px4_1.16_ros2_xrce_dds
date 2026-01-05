72841c6917c99c782e14006866630d16a9455a24
nidhi-aifi <nidhi.r@aifitechnologies.com>
2026-01-05 09:53:46 +0400

    feature: added starting location. Tested.

--- launch_files/single_x500_lidar_icp.sh
+++ launch_files/single_x500_lidar_icp.sh
@@ -7,6 +7,17 @@ PX4_SYS_AUTOSTART=4013  #x500_lidar_2d
 GZ_WORLD="default" #  use custom_indoor_lab
 PX4_UXRCE_DDS_PORT=8888
 
+# PX4 LOCAL STARTING LOCATION Local ENU coordinates (meters, radians)
+PX4_START_X=100.0     # East
+PX4_START_Y=0.0     # North
+PX4_START_Z=0.3     # Up (spawn slightly above ground)
+PX4_START_ROLL=0.0
+PX4_START_PITCH=0.0
+PX4_START_YAW=0.0   # radians (0 = facing East)
+
+PX4_GZ_MODEL_POSE="${PX4_START_X},${PX4_START_Y},${PX4_START_Z},${PX4_START_ROLL},${PX4_START_PITCH},${PX4_START_YAW}"
+
+
 # Kill existing session if it exists
 tmux kill-session -t $SESSION_NAME 2>/dev/null
 
@@ -22,7 +33,7 @@ tmux send-keys -t $SESSION_NAME:0.0 " bash -c ' python3 /home/ubuntu/PX4-gazebo-
 # Pane 1: PX4 SITL
 # ----------------------------
 tmux split-window -h -t $SESSION_NAME:0.0
-tmux send-keys -t $SESSION_NAME:0.1 "bash -c ' PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_PARAM_UXRCE_DDS_SYNCT=0 \
+tmux send-keys -t $SESSION_NAME:0.1 "bash -c ' PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_PARAM_UXRCE_DDS_SYNCT=0 PX4_GZ_MODEL_POSE=\"$PX4_GZ_MODEL_POSE\" \
 /home/ubuntu/px4_sitl/bin/px4 -w /home/ubuntu/px4_sitl/romfs ' " C-m
 
 # ----------------------------
