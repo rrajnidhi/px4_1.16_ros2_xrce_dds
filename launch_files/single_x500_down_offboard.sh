efddf4847ea97ef1dbc0e886b9dbe830fe5e8ced
nidhi-aifi <nidhi.r@aifitechnologies.com>
2026-01-13 10:53:12 +0400

    removing typo

--- launch_files/single_x500_down_offboard.sh
+++ launch_files/single_x500_down_offboard.sh
@@ -28,14 +28,14 @@ tmux new-session -d -s $SESSION_NAME
 # ----------------------------
 # Pane 0: Gazebo Simulation
 # ----------------------------
-tmux send-keys -t $SESSION_NAME:0.0 "  ' python3 /home/ubuntu/PX4-gazebo-models/simulation-gazebo --model_store /home/ubuntu/PX4-gazebo-models --world $GZ_WORLD ' " C-m
+tmux send-keys -t $SESSION_NAME:0.0 "  python3 /home/ubuntu/PX4-gazebo-models/simulation-gazebo --model_store /home/ubuntu/PX4-gazebo-models --world $GZ_WORLD  " C-m
 
 # ----------------------------
 # Pane 1: PX4 SITL
 # ----------------------------
 tmux split-window -h -t $SESSION_NAME:0.0
-tmux send-keys -t $SESSION_NAME:0.1 " ' PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_PARAM_UXRCE_DDS_SYNCT=0 PX4_GZ_MODEL_POSE=\"$PX4_GZ_MODEL_POSE\" \
-PX4_GZ_MODEL=$PX4_GZ_MODEL /home/ubuntu/px4_sitl/bin/px4 -w /home/ubuntu/px4_sitl/romfs ' " C-m
+tmux send-keys -t $SESSION_NAME:0.1 " PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=$PX4_SYS_AUTOSTART PX4_PARAM_UXRCE_DDS_SYNCT=0 PX4_GZ_MODEL_POSE=\"$PX4_GZ_MODEL_POSE\" \
+PX4_GZ_MODEL=$PX4_GZ_MODEL /home/ubuntu/px4_sitl/bin/px4 -w /home/ubuntu/px4_sitl/romfs  " C-m
 
 # ----------------------------
 # Pane 2: Gazebo → ROS 2 Bridge for Image transport
@@ -66,7 +66,7 @@ tmux select-layout -t $SESSION_NAME:0 tiled
 
 # ROS2 tools window
 tmux new-window -t $SESSION_NAME -n "ros2_tools"
-tmux send-keys -t $SESSION_NAME:1 " ' sleep 8;ros2 run rqt_image_view rqt_image_view' " C-m
+tmux send-keys -t $SESSION_NAME:1 "  sleep 8;ros2 run rqt_image_view rqt_image_view " C-m
 
 # Focus and attach
 tmux select-window -t $SESSION_NAME:0
